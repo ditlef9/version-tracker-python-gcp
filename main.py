226c492498c62afebf61edf0c7416dae72a7a8bc
Sindre Ditlefsen <sindre@cdcadvania.com>
2025-03-31 14:47:19 +0200

    V1

---
+++ main.py
@@ -0,0 +1,16 @@
+import flask
+import functions_framework
+
+@functions_framework.http
+def main(request: flask.wrappers.Request):
+    """ HTTP Cloud Functions """
+    log_headline: str = "main()"
+    print(f"{log_headline} · Init")
+
+if __name__ == '__main__':
+    print("version-tracker local run")
+
+    app = flask.Flask(__name__) # Creates a Flask app instance
+    request = flask.request
+    main(request)
+
